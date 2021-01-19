e85ebf2a94e6a340c12f465c2e2c184e95ca1704
Lorenzo Sottocorno <lori.sottocorno@libero.it>
2021-01-19 22:41:42 +0100

    Initial Commit

---
+++ Calcolatrice 1.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calcolatrice 1.0
+//
+//  Created by ifts14 on 19/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
